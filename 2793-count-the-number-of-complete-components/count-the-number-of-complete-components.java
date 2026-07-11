05f25c9b005ec6e14d8ebe37858e591d8feefa08
Shashank kumar <shashankkumar5901@gmail.com>
2026-07-11 21:58:30 +0530

    Time: 9 ms (82.85%) | Memory: 47.9 MB (20.28%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+
+    public int countCompleteComponents(int n, int[][] edges) {
+
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+
+                int[] info = new int[2]; // info[0] = nodes, info[1] = degree sum
+                dfs(i, graph, visited, info);
+
+                int nodes = info[0];
+                int edgeCount = info[1] / 2;
+
+                if (edgeCount == nodes * (nodes - 1) / 2) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(int node, List<Integer>[] graph, boolean[] visited, int[] info) {
+        visited[node] = true;
+        info[0]++;                     // count node
+        info[1] += graph[node].size(); // add degree
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next, graph, visited, info);
+            }
+        }
+    }
+}
