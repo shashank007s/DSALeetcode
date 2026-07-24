30d9de541df7b19c5f53949c3df7e5a8210382ac
Shashank kumar <shashankkumar5901@gmail.com>
2026-07-24 20:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> st;
+        for(char ch:s){
+            if(ch=='(' || ch== '{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else{
+                    char top = st.top();
+st.pop();
+                    if(
+                        (ch==')' && top!='(') 
+                    || (ch=='}' && top!='{' )||
+                    ( ch==']' && top!='[')){
+                        return false;
+
+                    }
+                }
+            }
+        }
+        return st.empty();  
+    }
+};
